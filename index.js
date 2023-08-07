9b1744c3d5f901d239e120035e93afafbd779837
Pulak <pulakkumarghosh2001@gmail.com>
2023-08-08 04:30:15 +0530

    Api updated

--- index.js
+++ index.js
@@ -4,20 +4,26 @@ const cors = require("cors");
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_CLIENT_SECRET);
 const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
+const cron = require('node-cron');
 const serviceAccount = require("./serviceDetails.json")
 
 const app = express();
 const PORT = 8080;
 
 app.use("/stripe", express.raw({type: "*/*"}))
-app.use(express.json());
+app.use(bodyParser.json())
 app.use(cors());
+
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.firestore().collection('game');
+let currentGameId = null;
+
+const db = admin.firestore().collection('games');
 const userDb = admin.firestore().collection('newData');
 const transactionDb = admin.firestore().collection('transactions')
 
@@ -62,172 +68,102 @@ app.post('/stripe', async (req, res) => {
     res.json({ok: true})
 })
 
-app.get('/check', async function (req,res) {
-    await db.where('active', '==', 1).get().then(async(query)=> {
-        const data = query.docs[0].data();
-        let tod = new Date();
-        let startMinute = data.id.slice(-2);
-        let startSecond = data.startSecond;
-        let nowtime = new Date();
-        let diff = (parseInt(nowtime.getMinutes()*60 + parseInt(nowtime.getSeconds()))) - (parseInt(startMinute)*60 + parseInt(startSecond) ) ;
-        
-        let result = {
-            id: data.id,
-            startSecond: data.startSecond,
-            diff: diff
-        }
-        res.json(result);
-    }).catch(err => {
-        res.status(502).json({error: err.message})
-    })
-    
-})
+const firestore = admin.firestore()
 
-var minute = 3, interval = minute*60*1000;
-
-setInterval(async function() {
-    var todays = new Date();
-    var date = todays.getDate();
-    var year = todays.getFullYear();
-    var month = todays.getMonth()+1;
-    var hour = todays.getHours();
-    var minute = todays.getMinutes();
-    if (month<10){
-        month=`0${month.toString()}`;
-    } else {
-        month = month.toString();
+const startGame = async () => {
+    try {
+      const gameSnapshot = await firestore.collection('games').orderBy('gameId', 'desc').limit(1).get();
+      let newGameId = 1;
+  
+      if (!gameSnapshot.empty) {
+        const lastGame = gameSnapshot.docs[0];
+        newGameId = lastGame.data().gameId + 1;
+      }
+  
+      await firestore.collection('games').doc(newGameId.toString()).set({
+        gameId: newGameId,
+        startTime: admin.firestore.FieldValue.serverTimestamp(),
+      });
+  
+      currentGameId = newGameId.toString();
+      console.log(`Game started with ID: ${currentGameId}`);
+    } catch (error) {
+      console.error('Error starting a new game:', error);
     }
+};
 
-    if(date<10){
-        date = `0${date.toString()}`;
-    } else {
-        date = date.toString();
-    }
-
-    if(hour<10){
-        hour = `0${hour.toString()}`;
-    } else {
-        hour = hour.toString();
-    }
-
-    if(minute<10){
-        minute = `0${minute.toString()}`;
-    } else {
-        minute = minute.toString();
-    }
-    year = year.toString();
-    var id = year+month+date+hour+minute;
-    console.log(id);
-    let arr = [];
-    for(let i=0; i<10;i++) {
-        let temp = {};
-        temp[i] = 0;
-        arr.push(temp);
-    }
-
-    let winNumber = 0;
-    let investData = [];
-    let win = '';
-    await db.where('active', '==', 1).get().then(async (query) => {
-        const docs = query.docs;
-        if(docs.length > 0) {
-            let dt = docs[0].data();
-            console.log(dt)
-            let red = dt.red;
-            investData = dt.investData;
-            let green = dt.green;
-            let winNumber_ = Math.floor(Math.random() * 3);
-            if(winNumber_ === 1) {
-                win = 'green';
-            } else if(winNumber_ === 2) {
-                win = 'red';
-            } else {
-                let choose = Math.floor(Math.random() * 2) + 1
-                if(choose === 1){
-                    win ='red violet'
-                } else {
-                    win = 'green violet'
-                }
-            }
-            let green_numbers = [1,3,7,9];
-            let red_numbers = [2,4,6,8];
-            let violet_numbers = [0,5];
-            if (win === 'green'){
-                winNumber = green_numbers[Math.floor(Math.random()*green_numbers.length)];
-            } else if (win === 'red'){
-                winNumber = red_numbers[Math.floor(Math.random()*red_numbers.length)];
-            } else if (win.includes('violet')){
-                if(win.includes('red')){
-                    winNumber = 0
-                } else {
-                    winNumber = 5
-                }
-            }
-            let price = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000
-            console.log(price);
-            docs[0].ref.update({
-                active: 0,
-                win: win,
-                price: price.toString()+winNumber.toString()
-            })
+const endGame = async () => {
+    try {
+      if (currentGameId) {
+        let winColor = '';
+        const randomNumber = Math.floor(Math.random() * 4)
+        let colors = ['Red', 'Green', 'Violet Red', 'Violet Green'];
+        let green_num = [1,3,7,9]
+        let red_num = [2,4,6,8]
+        let winNumber = 0
+        if (randomNumber === 0){
+            winNumber=red_num[Math.floor(Math.random()*4)]
+        } else if (randomNumber === 1){
+            winNumber = green_num[Math.floor(Math.random()*4)]
+        } else if(randomNumber === 2){
+            winNumber = 0
+        } else if(randomNumber === 3){
+            winNumber = 5
         }
-    })
-    let uploadData = {
-        id: id,
-        amount: 0,
-        investData: [],
-        red: 0,
-        green: 0,
-        violet: 0,
-        numberData: '',
-        win: '',
-        active: 1,
-        startSecond: todays.getSeconds()
-    };
-    // console.log(uploadData)
-    await db.add(uploadData);
-    for(let j = 0;j < investData.length;j++){
-        let reward = 0;
-        let personalData = investData[j];
-        if(winNumber === personalData.number){
-            reward = reward + personalData.amount*7;
-        }
-        if(personalData.color === 'Green' && win.includes('green')){
-            reward = reward + personalData.amount*1.8;
-        }
-        if(personalData.color === 'Red' && win.includes('red')){
-            reward = reward + personalData.amount*1.8;
-        }
-        if(personalData.color === 'Violet' && win.includes('violet')){
-            reward = reward + personalData.amount*3.5
-        }
-        const options = {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-            hour12: true,
-          };
-        let nowD = new Date().toLocaleString('en-IN', options).replace(',', '');
-        // var tt = `${nowD.getFullYear()}/${nowD.getMonth()+1}/${nowD.getDate()} ${nowD.getHours()}:${nowD.getMinutes()}:${nowD.getSeconds()}`
-        await userDb.where('phone', '==', personalData.phone).get().then(async(query)=> {
-            await query.docs[0].ref.update({
-                balance: query.docs[0].data().balance + reward
-            })
-            await transactionDb.add({
-                id: query.docs[0].id,
-                phone: query.docs[0].data().phone,
-                amount: reward,
-                time: nowD,
-                message: 'Success, Credited for Game'
-            })
-        })
-
-
+        const randomTwoDigitNumber = Math.floor(Math.random() * 90) + 10;
+        await firestore.collection('games').doc(currentGameId).update({
+          endTime: admin.firestore.FieldValue.serverTimestamp(),
+          winColor: colors[randomNumber],
+          winNumber: winNumber,
+          price: '135'+randomTwoDigitNumber.toString()+winNumber.toString()
+        });
+        console.log(`Game ended with ID: ${currentGameId}`);
+        currentGameId = null;
+      } else {
+        console.log('No active game to end.');
+      }
+    } catch (error) {
+      console.error('Error ending the game:', error);
     }
+};
 
-}, interval);
+cron.schedule('*/3 * * * *', () => {
+    endGame();
+    startGame();
+});
+
+app.get('/currentGame', async (req, res) => {
+    try {
+      if (currentGameId) {
+        // Calculate the remaining time for the current game
+        const currentGameSnapshot = await firestore.collection('games').doc(currentGameId).get();
+        const currentGameData = currentGameSnapshot.data();
+        const startTime = currentGameData.startTime.toDate().getTime();
+        const currentTime = Date.now();
+        const remainingTimeInSeconds = Math.floor((startTime + 180000 - currentTime) / 1000);
+  
+        res.status(200).json({ gameId: currentGameId, remainingTime: remainingTimeInSeconds });
+      } else {
+        // If there is no active game, start a new one
+        startGame();
+        res.status(200).json({ gameId: currentGameId, remainingTime: 180 });
+      }
+    } catch (error) {
+      console.error('Error fetching game data:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('/previousGames', async (req, res) => {
+    try {
+      const previousGamesSnapshot = await firestore.collection('games').where('endTime', '!=', null).get();
+      const previousGames = previousGamesSnapshot.docs.map((doc) => doc.data());
+  
+      res.status(200).json(previousGames);
+    } catch (error) {
+      console.error('Error fetching previous games:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 app.listen(PORT, ()=> console.log('Server running on port 8080'))
