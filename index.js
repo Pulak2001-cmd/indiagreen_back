212c92b6b28ec84f96c396122ef4fb8f0fcd8e53
Pulak <pulakkumarghosh2001@gmail.com>
2023-08-28 20:29:28 +0530

    New version

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ app.get('/previousGames', async (req, res) => {
 
 app.get('/currentVersion', async (req, res) => {
   const obj = {
-    version: '2.0.0'
+    version: '3.0.0'
   }
   res.status(200).json(obj);
 })
